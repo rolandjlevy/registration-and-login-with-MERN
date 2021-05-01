bb5fba1acd61b48c3e9649e4bc8a6868db783941
rolandjlevy <roland.levy@ontrackms.com>
2021-05-01 01:56:54 +0100

    Basic form working

--- server.js
+++ server.js
@@ -32,32 +32,57 @@ const UserSchema = new mongoose.Schema({
 });
 const User = mongoose.model('User', UserSchema);
 
-// saving data into db
-const data = {
-    title: 'Welcome to the test',
-    body: 'This is a full-stack test using Node'
-};
-
-// instance of model
-const newUser = new User(data);
-
-newUser.save((error) => {
-    if (error) {
-        console.log({error});
-    } else {    
-        console.log('data has been saved');
-    }
-})
-
 // Routes
+
 app.get('/', (req, res) => {
+    res.send(`
+        <h1>Home</h1>
+        <p><a href="/api/display">Display data</a></p>
+        <form method="post" action="/api/save">
+        <p><input name="title" placeholder="Title..." /></p>
+        <p><input name="body" placeholder="Body..." /></p>
+        <p><button type="submit">Save data</button></p>
+    `);
+});
+
+app.get('/api/display', (req, res) => {
     User.find({  })
     .then(data => {
-        // console.log('Data: ', data);
-        res.json(data);
+        let str = '';
+        data.forEach(user => {
+            str += '<ul>';
+            str += `<li>Date: ${user.date}</li>`;
+            str += `<li>Title: ${user.title}</li>`;
+            str += `<li>Body: ${user.body}</li>`;
+            str += '</ul>';
+        });
+        str += '<p><a href="/">Home</a></p>';
+        res.send(str);
     })
     .catch(error => {
         console.log('error: ', error);
+    });
+});
+
+app.post('/api/save', (req, res) => {
+    // saving data into db
+    const { title, body } = req.body;
+    const data = {
+        title,
+        body
+    };
+    // instance of model
+    const newUser = new User(data);
+    newUser.save((error) => {
+        if (error) {
+            console.log({error});
+        } else {
+            res.send(`
+                <h1>Saved</h1>
+                <p><a href="/api/display">Display data</a></p>
+                <p><a href="/">Home</a></p>
+            `);
+        }
     });
 });
 
